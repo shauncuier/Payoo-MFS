d7919a0cb85ad93677b393abe9cf1a3847336e3e
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-25 23:48:31 +0600

    implement login functionality with validation in main.js and set default values in index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-document.getElementById('login-btn').addEventListener('click', function () {
-        const accountNumber = document.getElementById('accountNumber').value;
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    const accountNumber = document.getElementById('accountNumber').value;
+
+    const accountPin = document.getElementById('accountPin').value;
+
+    const pin = parseInt(accountPin);
+
+    if (accountNumber.length === 11) {
+        if (pin === 1234) {
+            // redirect to dashboard
+            window.location.href = 'main.html';
+            
+        }
+        else {
+            alert('Incorrect Pin');
+        }
+    } else {
+        alert('Account Number must be 11 digits');
+    }
+
+
+
+    
 })
