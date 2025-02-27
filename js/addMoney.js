322f7db66a14f4699697cf14ccf127ffbff49510
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-27 23:58:39 +0600

    update addMoney.js to improve money addition logic and validation; modify main.html to display balance dynamically

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,42 +1,34 @@
-document.getElementById("addMoneyBtn").addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    const amount = document.getElementById("money").value;
+document
+    .getElementById("addMoneyBtn")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
 
-    const money = parseInt(amount);
+        const amount = document.getElementById("money").value;
 
-    console.log(typeof money, money);
-
-
+        const money = parseInt(amount);
 
 
+        const accountNumber = document.getElementById("accountNumber").value;
 
+        const accountPin = document.getElementById("accountPin").value;
 
+        const pin = parseInt(accountPin);
 
+        const mainBalance = document.getElementById("mainBalance").innerText;
+                const balance = parseFloat(mainBalance);
 
+        if (accountNumber.length === 11) {
+            if (pin === 1234) {
+                
+                const newBalance = balance + money;
 
+                document.getElementById("mainBalance").innerText= newBalance;
 
-
-
-    const accountNumber = document.getElementById('accountNumber').value;
-
-    const accountPin = document.getElementById('accountPin').value;
-
-    const pin = parseInt(accountPin);
-
-    if (accountNumber.length === 11) {
-        if (pin === 1234) {
-            // redirect to dashboard
-            // window.location.href = 'main.html';
-            alert('Money Added Successfully');
-            
+                console.log(newBalance);
+            } else {
+                alert("Incorrect Pin");
+            }
+        } else {
+            alert("Account Number must be 11 digits");
         }
-        else {
-            alert('Incorrect Pin');
-        }
-    } else {
-        alert('Account Number must be 11 digits');
-    }
-    
-    
-});
+    });
